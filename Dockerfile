09d19dc35079add65af06405e41dbd11d7c038f0
Evan Hazlett <ejhazlett@gmail.com>
2014-02-02 15:54:43 -0500

    add support for agent container and automatic registration

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 from ubuntu:12.04
 maintainer Shipyard Project "http://shipyard-project.com"
 run apt-get update
-run apt-get install -y curl
-run curl https://get.docker.io/builds/Linux/x86_64/docker-latest -o /usr/local/bin/docker
+run apt-get install -y curl python-simplejson
+run curl --silent https://get.docker.io/builds/Linux/x86_64/docker-latest -o /usr/local/bin/docker
 run chmod +x /usr/local/bin/docker
 add run.sh /usr/local/bin/run
 entrypoint ["/usr/local/bin/run"]
--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ if [ -z "$ACTION" ] ; then
 fi
 
 function cleanup {
-    for CNT in shipyard shipyard_redis shipyard_router shipyard_db shipyard_lb
+    for CNT in shipyard shipyard_redis shipyard_router shipyard_db shipyard_lb shipyard_agent
     do
         docker -H unix://docker.sock kill $CNT > /dev/null 2>&1
         docker -H unix://docker.sock rm $CNT > /dev/null 2>&1
@@ -31,21 +31,33 @@ function cleanup {
 
 function purge {
     cleanup
-    for IMG in shipyard redis router db lb
+    for IMG in shipyard redis router db lb agent
     do
         docker -H unix://docker.sock rmi shipyard/$IMG > /dev/null 2>&1
     done
 }
 
 if [ "$ACTION" = "setup" ] ; then
-    echo "
-Using $TAG tag for Shipyard
+    echo "Using $TAG tag for Shipyard
 This may take a moment while the Shipyard images are pulled..."
     redis=$(docker -H unix://docker.sock run -i -t -d -p 6379:6379 -name shipyard_redis shipyard/redis)
     router=$(docker -H unix://docker.sock run -i -t -d -p 80 -link shipyard_redis:redis -name shipyard_router shipyard/router)
     lb=$(docker -H unix://docker.sock run -i -t -d -p 80:80 -link shipyard_redis:redis -link shipyard_router:app_router -name shipyard_lb shipyard/lb)
     db=$(docker -H unix://docker.sock run -i -t -d -p 5432 -e DB_PASS=$DB_PASS -name shipyard_db shipyard/db)
     shipyard=$(docker -H unix://docker.sock run -i -t -d -p 8000:8000 -link shipyard_db:db -link shipyard_redis:redis -name shipyard -e ADMIN_PASS=$ADMIN_PASS shipyard/shipyard:$TAG app master-worker)
+    echo "Configuring Shipyard.  One moment please..."
+    # wait for shipyard to start before registering
+    until `curl --output /dev/null --silent --head --fail "http://172.17.42.1:8000"`; do
+        sleep 1
+    done
+    KEY=$(curl -d "username=admin&password=shipyard" --silent http://172.17.42.1:8000/api/login | python -c "import sys,json ; data=json.loads(sys.stdin.read()) ; print(data['api_key'])")
+    agent=$(docker -H unix://docker.sock run -i -t -d -v /var/run/docker.sock:/docker.sock -p 4500 -name shipyard_agent shipyard/agent -url http://172.17.42.1:8000 -docker /docker.sock -register)
+    # wait until host is registered
+    until `curl --max-time 5 --output /dev/null --silent --fail -H "Accept: application/json" -H "Authorization: ApiKey admin:$KEY" http://172.17.42.1:8000/api/v1/hosts/1/`; do
+        sleep 1
+    done
+    # set host to enabled
+    curl --silent -H "Content-type: application/json" -H "Authorization: ApiKey admin:$KEY" -XPATCH --data '{ "enabled": true}' http://172.17.42.1:8000/api/v1/hosts/1/
     echo "
 Shipyard Stack Deployed
 
