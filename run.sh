ac3c0cdd5db44faeb574dd3bd6db32f1e171e466
Evan Hazlett <ejhazlett@gmail.com>
2014-05-02 09:25:58 -0400

    fixes #11: use absolute paths for socket

--- run.sh
+++ run.sh
@@ -25,8 +25,8 @@ fi
 function cleanup {
     for CNT in shipyard shipyard_redis shipyard_router shipyard_db shipyard_lb
     do
-        docker -H unix://docker.sock kill $CNT > /dev/null 2>&1
-        docker -H unix://docker.sock rm $CNT > /dev/null 2>&1
+        docker -H unix:///docker.sock kill $CNT > /dev/null 2>&1
+        docker -H unix:///docker.sock rm $CNT > /dev/null 2>&1
     done
 }
 
@@ -34,7 +34,7 @@ function purge {
     cleanup
     for IMG in shipyard redis router db lb
     do
-        docker -H unix://docker.sock rmi shipyard/$IMG > /dev/null 2>&1
+        docker -H unix:///docker.sock rmi shipyard/$IMG > /dev/null 2>&1
     done
 }
 
@@ -43,23 +43,23 @@ if [ "$ACTION" = "setup" ] ; then
 Using $TAG tag for Shipyard
 This may take a moment while the Shipyard images are pulled..."
     echo "Starting Redis..."
-    redis=$(docker -H unix://docker.sock run -i -t -d -p 6379:6379 --name shipyard_redis shipyard/redis)
+    redis=$(docker -H unix:///docker.sock run -i -t -d -p 6379:6379 --name shipyard_redis shipyard/redis)
     sleep 2
     echo "Starting App Router..."
-    router=$(docker -H unix://docker.sock run -i -t -d -p 80 --link shipyard_redis:redis --name shipyard_router shipyard/router)
+    router=$(docker -H unix:///docker.sock run -i -t -d -p 80 --link shipyard_redis:redis --name shipyard_router shipyard/router)
     sleep 2
     echo "Starting Load Balancer..."
-    lb=$(docker -H unix://docker.sock run -i -t -d -p 80:80 --link shipyard_redis:redis --link shipyard_router:app_router --name shipyard_lb shipyard/lb)
+    lb=$(docker -H unix:///docker.sock run -i -t -d -p 80:80 --link shipyard_redis:redis --link shipyard_router:app_router --name shipyard_lb shipyard/lb)
     sleep 2
     echo "Starting DB..."
     EXTRA_DB_ARGS=""
     if [ ! -z "$DB_HOST_VOLUME" ] ; then
         EXTRA_DB_ARGS="-v $DB_HOST_VOLUME:/var/lib/postgresql"
     fi
-    db=$(docker -H unix://docker.sock run -i -t -d -p 5432 -e DB_NAME=shipyard -e DB_USER=shipyard -e DB_PASS=$DB_PASS $EXTRA_DB_ARGS --name shipyard_db shipyard/db)
+    db=$(docker -H unix:///docker.sock run -i -t -d -p 5432 -e DB_NAME=shipyard -e DB_USER=shipyard -e DB_PASS=$DB_PASS $EXTRA_DB_ARGS --name shipyard_db shipyard/db)
     sleep 5
     echo "Starting Shipyard"
-    shipyard=$(docker -H unix://docker.sock run -i -t -d -p 8000:8000 --link shipyard_db:db --link shipyard_redis:redis --name shipyard -e ADMIN_PASS=$ADMIN_PASS -e DEBUG=$DEBUG --entrypoint /app/.docker/run.sh shipyard/shipyard:$TAG app master-worker)
+    shipyard=$(docker -H unix:///docker.sock run -i -t -d -p 8000:8000 --link shipyard_db:db --link shipyard_redis:redis --name shipyard -e ADMIN_PASS=$ADMIN_PASS -e DEBUG=$DEBUG --entrypoint /app/.docker/run.sh shipyard/shipyard:$TAG app master-worker)
     echo "
 Shipyard Stack Deployed
 
@@ -71,21 +71,21 @@ elif [ "$ACTION" = "restart" ] ; then
     for CNT in redis router lb db
     do
         echo "Starting $CNT"
-        docker -H unix://docker.sock start shipyard_$CNT > /dev/null
+        docker -H unix:///docker.sock start shipyard_$CNT > /dev/null
     done
     echo "Starting shipyard"
-    docker -H unix://docker.sock stop shipyard > /dev/null
-    docker -H unix://docker.sock start shipyard > /dev/null
+    docker -H unix:///docker.sock stop shipyard > /dev/null
+    docker -H unix:///docker.sock start shipyard > /dev/null
 elif [ "$ACTION" = "upgrade" ] ; then
     echo "Pulling latest Shipyard"
-    docker -H unix://docker.sock pull shipyard/shipyard
+    docker -H unix:///docker.sock pull shipyard/shipyard
 
     echo "Stopping and removing existing Shipyard container"
-    docker -H unix://docker.sock kill shipyard > /dev/null
-    docker -H unix://docker.sock rm shipyard > /dev/null
+    docker -H unix:///docker.sock kill shipyard > /dev/null
+    docker -H unix:///docker.sock rm shipyard > /dev/null
 
     echo "Starting new Shipyard container"
-    docker -H unix://docker.sock run -i -t -d -p 8000:8000 --link shipyard_db:db --link shipyard_redis:redis --name shipyard -e DEBUG=$DEBUG --entrypoint /app/.docker/run.sh shipyard/shipyard:$TAG app master-worker > /dev/null
+    docker -H unix:///docker.sock run -i -t -d -p 8000:8000 --link shipyard_db:db --link shipyard_redis:redis --name shipyard -e DEBUG=$DEBUG --entrypoint /app/.docker/run.sh shipyard/shipyard:$TAG app master-worker > /dev/null
 elif [ "$ACTION" = "cleanup" ] ; then
     cleanup
     echo "Shipyard Stack Removed"
